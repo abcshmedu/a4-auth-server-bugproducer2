20f7c40ffe9341ef2969e3679d3b9792334bcc83
Johdeath <johannes.arzt@gmx.net>
2017-05-20 09:11:24 +0200

    add testLoginWithVaildData() => sadly faild

--- src/test/java/edu/hm/bugproducer/RestApiTest.java
+++ src/test/java/edu/hm/bugproducer/RestApiTest.java
@@ -2,21 +2,39 @@ package edu.hm.bugproducer;
 
 import edu.hm.bugproducer.restAPI.TokenUtils;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.apache.http.HttpHeaders.USER_AGENT;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class RestApiTest {
 
+
+    //URL
     private static final String URL = "http://localhost:8082";
+    private static final String URLLOGIN = "http://localhost:8082/login/";
+
+    //User
+    private static final String USER = "John Doe";
+    private static final String PASSWORD = "geheim";
+
+    private static final String CORRUPTUSER = "Jane Doe";
+    private static final String CORRUPTPASSWORD = "offen";
 
 
     private JettyStarter jettyStarter;
@@ -45,6 +63,29 @@ public class RestApiTest {
 
         System.out.println("Response Code : "
                 + response.getStatusLine().getStatusCode());
+    }
+
+    @Test
+    public void testLoginWithVaildData() throws IOException {
+        List<NameValuePair> userData = new ArrayList<>();
+
+        userData.add(new BasicNameValuePair("user",USER));
+        userData.add(new BasicNameValuePair("password",PASSWORD));
+
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost loginToWebsite = new HttpPost(URLLOGIN);
+
+        loginToWebsite.setEntity(new UrlEncodedFormEntity(userData));
+        loginToWebsite.addHeader("content-Type", "application/x-www-form-urlencoded");
+        HttpResponse response =client.execute(loginToWebsite);
+
+        assertEquals(200,response.getStatusLine().getStatusCode());
+
+        System.err.print("TOKEN: "+response.getEntity().toString());
+
+
+
     }
 
 
